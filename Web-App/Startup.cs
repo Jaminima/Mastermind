bf374bc9a9a60dc571f04961ef7c9541226ec534
Oscar Davies <oscar.william.davies@outlook.com>
2021-04-16 17:00:33 +0100

    Properly Store Current Round

--- Web-App/Startup.cs
+++ Web-App/Startup.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Mastermind;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 
 namespace Better_Razor_Server_Template
 {
